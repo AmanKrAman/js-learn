1729fe28783e1afa198966ee1d83783bcd162542
AmanKrAman <96979867+AmanKrAman@users.noreply.github.com>
2024-01-05 02:05:10 +0000

    conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = 0
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true = 1; false = 0;
+
+let isloggedIn = 1
+let isBooleanLogedIn = Boolean(isloggedIn)
+console.log(isBooleanLogedIn);
+
+// 0=>false 1=> true
+//"" => false
+//"value" => true
+
+let someNumber = 33
+let stringNum = String(someNumber)
+console.log(stringNum); 
+console.log(typeof stringNum); 
