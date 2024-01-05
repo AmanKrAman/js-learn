c5f303690b0e868a5ee3b67979ba7e23b1c35785
AmanKrAman <96979867+AmanKrAman@users.noreply.github.com>
2024-01-05 15:17:55 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,7 +13,7 @@ let valueInNumber = Number(score)
 
 let isloggedIn = 1
 let isBooleanLogedIn = Boolean(isloggedIn)
-console.log(isBooleanLogedIn);
+// console.log(isBooleanLogedIn);
 
 // 0=>false 1=> true
 //"" => false
@@ -21,5 +21,37 @@ console.log(isBooleanLogedIn);
 
 let someNumber = 33
 let stringNum = String(someNumber)
-console.log(stringNum); 
-console.log(typeof stringNum); 
+// console.log(stringNum); 
+// console.log(typeof stringNum); 
+
+
+/* ****************Operations ************* */
+//mathermatical operation are also there
+// console.log(8+5);
+// console.log(8-5); //etc
+
+let str1 = "Hello "
+let str2 = "World "
+let str3 = "Aman Here!"
+// console.log(str1+str2+str3);
+
+// console.log( 5 + "10");
+// console.log("aman" + 10);
+// console.log("10"+ "20");
+// console.log("10" + 5 + 6); 
+// console.log(5 + 6 + "aman");
+// console.log("aman "+"Kumar");
+
+// console.log(true);
+// console.log(+true);
+console.log(+"");
+
+let num1 , num2, num3;
+num1 = num2 = num3 = 20+30;
+// console.log(num1);
+let gamecontroller = 100;
+// gamecontroller++;
+let num = 10;
+++num;
+console.log(++gamecontroller); 
+console.log(num);
