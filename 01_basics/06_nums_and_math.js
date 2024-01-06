3ba691f01d353b77e12c7a0f8be2ecbdf39fba45
AmanKrAman <96979867+AmanKrAman@users.noreply.github.com>
2024-01-06 11:25:51 +0000

    math_and_nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 500
+// console.log(score);
+const balance = new Number (200)
+// console.log(balance);
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const Num = 1085.8966
+// console.log(Num.toFixed(2));
+// console.log(Num.toPrecision(3));
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-In')); 
+// without en-In it'll show in us standard
+/* ***************Maths********************* */
+
+
+// console.log(Math);
+// console.log(Math.abs(-10));
+// console.log(Math.round(8.3));
+// console.log(Math.ceil(8.5));
+// console.log(Math.floor(8.5));
+// console.log(Math.min(4,5,6,8));
+// console.log(Math.max(4,5,6,8));
+
+// console.log(Math.random());  //t gives value under 0 and 1
+// console.log((Math.random() *10) + 1);
+// console.log(Math.floor(Math.random() *10) +1);
+
+const min =  100
+const max = 200
+console.log(Math.floor(Math.random() * (max -min+1))+min);
+
