803b8d7ff5c318872167a5241269bde5f8f1ef34
AmanKrAman <96979867+AmanKrAman@users.noreply.github.com>
2024-01-08 09:40:38 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,52 @@
+// Run : node 03_basics/03_arrow.js
+
+
+const user = {
+    username : "Aman",
+    price : 1999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} ,  Welcome to the website.`);
+        console.log(this);
+    }
+}
+// user.welcomeMessage();
+// user.username= "ajay"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function fun(){
+//     let username = "Aman"
+//     // console.log(this);
+//     // console.log(this.username);
+// }
+// // fun()
+
+// const val = function () {
+//     let username = "Aman"
+//     console.log(this.username);
+// }
+// val()
+
+const val =  () => {
+    let username = "Aman"
+    // console.log(this.username);
+    console.log(this);
+}
+// val()
+
+//  Arrow function : ()=>{}
+
+// const addTwo = (num1 , num2) => {
+//     return num1+num2      // Explicit return , here return word required
+// }
+// console.log(addTwo(8,9));
+
+// const addTwo = (num1 , num2) =>  num1 + num2    //implicit return
+// const addTwo = (num1 , num2) =>  (num1 + num2)
+// const addTwo = (num1 , num2) =>  {return num1 + num2}
+const addTwo = (num1 , num2) =>  ({username : "Aman"})
+console.log(addTwo(8,9));
+
+// if you are using curly braces in implicit return return or parenthsis required
