f35d31bc9b03560c07c9d98160b2632d7212e77d
AmanKrAman <96979867+AmanKrAman@users.noreply.github.com>
2024-01-09 15:29:36 +0000

    controlflow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,15 @@
+//Run:  node 04_control_flow/one.js
+
+//if
+
+// const isuserloggedin = true
+// if(isuserloggedin){
+//     console.log("Logged In");
+// }
+
+const temperature = 52
+if(temperature < 50){
+    console.log("lesser than 50");
+}else{
+    console.log("Not less than 50");
+}
